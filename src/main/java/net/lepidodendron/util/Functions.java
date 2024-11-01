c7bcf85f49601e613c4c15dea24197b884f18525
hampop <87606666+hampop@users.noreply.github.com>
2024-11-01 17:01:31 +0000

    Revert "Update Functions.java"
    This reverts commit b3c95661c67bc3f92b3f0fa3bb4197d9c067bd95.

--- src/main/java/net/lepidodendron/util/Functions.java
+++ src/main/java/net/lepidodendron/util/Functions.java
@@ -216,9 +216,7 @@ public class Functions {
             IBlockState upState = worldIn.getBlockState(upPos);
             IBlockState downState = worldIn.getBlockState(downPos);
 
-            BlockDoublePlant.EnumBlockHalf currentHalf = currentState.getValue(BlockDoublePlant.HALF);
-
-            if (currentHalf == BlockDoublePlant.EnumBlockHalf.LOWER) {
+            if (currentState.getValue(BlockDoublePlant.HALF) == BlockDoublePlant.EnumBlockHalf.LOWER) {
                 if (upState.getBlock() instanceof BlockDoublePlant && upState.getValue(BlockDoublePlant.HALF) == BlockDoublePlant.EnumBlockHalf.UPPER) {
                     worldIn.setBlockToAir(upPos);
                 }
